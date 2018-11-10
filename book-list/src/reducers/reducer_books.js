208ee8068f1e5264c78c811ae507d9fe1140c3bb
Kento75 <kento2github@gmail.com>
2018-11-10 19:37:21 +0900

    Add redux and reuducer and ActionCreator

---
+++ book-list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "JavaScript : The Good Parts", pages: 123 },
+    { title: "Harry Potter", pages: 80 },
+    { title: "The Dark Tower", pages: 120 },
+    { title: "Eloquent Ruby", pages: 10 },
+  ]
+}
